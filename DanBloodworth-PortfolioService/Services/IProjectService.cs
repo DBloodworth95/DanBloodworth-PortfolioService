c84692559141a972756669223a8144b8b323a786
DBloodworth95 <dan.bloodworth95@gmail.com>
2022-01-23 17:15:28 +0000

    Add GetGitHubRepositories contract

--- DanBloodworth-PortfolioService/Services/IProjectService.cs
+++ DanBloodworth-PortfolioService/Services/IProjectService.cs
@@ -1,7 +1,11 @@
-﻿namespace DanBloodworth_PortfolioService.Services
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace DanBloodworth_PortfolioService.Services
 {
     public interface IProjectService
     {
-        
+        Task<IReadOnlyList<Repository>> GetGitHubRepositories(string apiKey);
     }
 }
