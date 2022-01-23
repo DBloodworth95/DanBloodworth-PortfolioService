2a6d631901750c92425ee779f598445ce51249cb
DBloodworth95 <dan.bloodworth95@gmail.com>
2022-01-23 18:47:13 +0000

    Add project service settings & fetch repositories

--- DanBloodworth-PortfolioService/Services/ProjectService.cs
+++ DanBloodworth-PortfolioService/Services/ProjectService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using DanBloodworth_PortfolioService.Providers;
+using DanBloodworth_PortfolioService.Settings;
+using Microsoft.Extensions.Options;
 using Octokit;
 
 namespace DanBloodworth_PortfolioService.Services
@@ -8,16 +10,19 @@ namespace DanBloodworth_PortfolioService.Services
     public class ProjectService : IProjectService
     {
         private readonly IGitHubClientFactory _gitHubClientFactory;
+        private readonly IOptions<ProjectServiceSettings> _options;
 
-        public ProjectService(IGitHubClientFactory gitHubClientFactory)
+        public ProjectService(IGitHubClientFactory gitHubClientFactory, 
+            IOptions<ProjectServiceSettings> options)
         {
             _gitHubClientFactory = gitHubClientFactory;
+            _options = options;
         }
 
         public async Task<IReadOnlyList<Repository>> GetGitHubRepositories(string apiKey)
         {
-            var client = _gitHubClientFactory.CreateClient(apiKey);
-            var repositories = await client.Repository.GetAllPublic();
+            var client = _gitHubClientFactory.CreateClient(_options.Value.ApiKey);
+            var repositories = await client.Repository.GetAllForCurrent();
 
             return repositories;
         }
---
+++ DanBloodworth-PortfolioService/Settings/ProjectServiceSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace DanBloodworth_PortfolioService.Settings
+{
+    public class ProjectServiceSettings
+    {
+        public string ApiKey { get; set; }
+
+        public ProjectServiceSettings()
+        {
+            ApiKey = "NoKeyFound";
+        }
+    }
+}
