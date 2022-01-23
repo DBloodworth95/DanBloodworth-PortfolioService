f4c37fd4dd0357cc60ba1ebab639188fbc4e332a
DBloodworth95 <dan.bloodworth95@gmail.com>
2022-01-23 16:43:19 +0000

    Add GitHubClientFactory

---
+++ DanBloodworth-PortfolioService/Providers/GitHubClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DanBloodworth_PortfolioService.Settings;
+using Microsoft.Extensions.Options;
+using Octokit;
+
+namespace DanBloodworth_PortfolioService.Providers
+{
+    public class GitHubClientFactory : IGitHubClientFactory
+    {
+        private readonly IOptions<GitHubClientSettings> _options;
+
+        public GitHubClientFactory(IOptions<GitHubClientSettings> options)
+        {
+            _options = options;
+        }
+
+        public IGitHubClient CreateClient(string apiKey)
+        {
+            return new GitHubClient(new ProductHeaderValue(_options.Value.ApplicationName),
+                new Uri(_options.Value.ServerUrl))
+            {
+                Credentials = new Credentials(apiKey)
+            };
+        }
+    }
+}
---
+++ DanBloodworth-PortfolioService/Providers/IGitHubClientFactory.cs
@@ -0,0 +1,9 @@
+﻿using Octokit;
+
+namespace DanBloodworth_PortfolioService.Providers
+{
+    public interface IGitHubClientFactory
+    {
+        public IGitHubClient CreateClient(string apiKey);
+    }
+}
