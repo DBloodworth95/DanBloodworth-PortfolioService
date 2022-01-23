64b4db62d05e3855cb9a775d0745356a0f167edd
DBloodworth95 <dan.bloodworth95@gmail.com>
2022-01-23 16:42:31 +0000

    Add GitHubClient options binding

---
+++ DanBloodworth-PortfolioService/Settings/GitHubClientSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace DanBloodworth_PortfolioService.Settings
+{
+    public class GitHubClientSettings
+    {
+        public string ServerUrl { get; set; }
+        public string ApplicationName { get; set; }
+
+        public GitHubClientSettings(string serverUrl, string applicationName)
+        {
+            ServerUrl = serverUrl;
+            ApplicationName = applicationName;
+        }
+    }
+}
